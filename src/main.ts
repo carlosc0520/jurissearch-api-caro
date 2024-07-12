f818860c3ed800b476508bb3f00f3ad77417d06b
ccarbajal <ccarbajalmt0520@gmail.com>
2024-07-12 11:30:21 -0500

    act2

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log(`Server running on ${3001}`)
+  console.log(`Server running on ${3000}`)
 
   // aumentar memoria de node;
   
 
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
