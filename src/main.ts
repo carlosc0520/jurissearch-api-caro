f1e6a448b12be6166c0befcca7c2edc8c6e27fe6
carlosc0520 <ccarbajalmt0520@gmail.com>
2024-09-30 15:15:23 -0500

    act

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   dotenv.config(); // Cargar las variables de entorno desde el archivo .env
 
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));  // Para JSON
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));  // Para URL-encoded data
+
   app.enableCors({
     origin: 'https://jurissearch.com', // Cambia esto por tu dominio
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
